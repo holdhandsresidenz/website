3e782976f686e51e14e7817de34342bb9b768f6b
Jonathan Schneider <js18hoju@studserv.uni-leipzig.de>
2021-02-22 21:26:13 +0100

    Zustand Open Call

---
+++ src/components/OpenCall.vue
@@ -0,0 +1,42 @@
+<template>
+
+    <a
+        target="_blank"
+        href="../assets/opencall-holdhands-de.pdf">
+      <img id="pdf-thumb" src="../assets/opencall-pdf-icon.png">
+    </a>
+
+</template>
+
+<script>
+export default {
+name: "OpenCall"
+}
+</script>
+
+<style scoped>
+a {
+  position: relative;
+  float: left;
+  width: 35vw;
+  height: 35vw;
+  padding-top: 31vh;
+  padding-left: 2vw;
+}
+#pdf-thumb{
+  height: 100px;
+}
+
+@media all and (max-width: 1000px) {
+  a {
+    position: relative;
+    width: 100vw;
+    height: 50vw;
+    margin-top: 30vh;
+    padding-left: 22vw;
+  }
+  #pdf-thumb{
+    height: 300px;
+  }
+}
+</style>
