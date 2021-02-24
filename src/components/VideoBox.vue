f568ca4894c294681dee711682a1bd9ba44344a7
Jonathan Schneider <js18hoju@studserv.uni-leipzig.de>
2021-02-24 19:33:40 +0100

    videoboxmodal entwurf

--- src/components/VideoBox.vue
+++ src/components/VideoBox.vue
@@ -3,6 +3,18 @@
       height="80"
       width="90"
   >
+    <button
+        type="button"
+        class="btn"
+        @click="showModal"
+    >
+      Open Modal!
+    </button>
+
+    <VideoBoxModal
+        v-show="isModalVisible"
+        @close="closeModal"
+    />
 <div class="box">
   <div v-bind:class="boxContent" >
     Hallo Test Text Video Beschreibung Halloo
@@ -14,18 +26,27 @@
 
 <script>
 import DraggableLink from "@/shared/DraggableLink";
+import VideoBoxModal from "@/shared/VideoBoxModal";
 
 export default {
   name: "VideoBox",
   components: {
-    DraggableLink
+    DraggableLink,
+    VideoBoxModal
   },
   data: function () {
     return {
-     boxContent: "textBox"
+     boxContent: "textBox",
+      isModalVisible: false,
     }
   },
   methods: {
+    showModal() {
+      this.isModalVisible = true;
+    },
+    closeModal() {
+      this.isModalVisible = false;
+    },
     switchContent: function () {
       setInterval(()=>
               this.boxContent = ((this.boxContent==="textBox") ? "pictureBox" : "textBox")
@@ -39,6 +60,36 @@ export default {
 </script>
 
 <style scoped>
+.modal-mask {
+  position: fixed;
+  z-index: 9998;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.5);
+  display: table;
+  transition: opacity 0.3s ease;
+}
+
+.modal-wrapper {
+  display: table-cell;
+  vertical-align: middle;
+}
+
+.modal-container {
+  width: 300px;
+  margin: 0px auto;
+  padding: 20px 30px;
+  background-color: #fff;
+  border-radius: 2px;
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.33);
+  transition: all 0.3s ease;
+  font-family: Helvetica, Arial, sans-serif;
+}
+
+
+
 .box {
   height: 100%;
   width: 100%;
